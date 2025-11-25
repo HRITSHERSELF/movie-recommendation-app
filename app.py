5faa0a7054da4517df68165e35ceb1323908349a
HRITSHERSELF <161748578+HRITSHERSELF@users.noreply.github.com>
2025-11-25 23:23:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+
+import streamlit as st
+import pandas as pd
+import pickle
+import requests
+
+# --------------------------
+# LOAD DATA
+# --------------------------
+
+movies = pickle.load(open("movies.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+movies_list = movies['title'].values
+
+# --------------------------
+# GET MOVIE POSTER FUNCTION
+# --------------------------
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=72a5bd467f3b0d9442e5d4021ba38d4b"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500" + poster_path
+    return "https://via.placeholder.com/500x750?text=No+Image"
+
+
+# --------------------------
+# RECOMMENDATION FUNCTION
+# --------------------------
+
+
